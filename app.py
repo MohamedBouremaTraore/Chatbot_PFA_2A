46b3cc56dd9cf6b99ca3ab497dbd74150c24f8a7
MohamedBouremaTraore <momotraore1513@gmail.com>
2022-04-13 19:45:32 +0200

    hello world

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'This project aim to create a chateboot for a virtual bank using flask'
+    return 'Hello world'
 
 
 if __name__ == '__main__':
